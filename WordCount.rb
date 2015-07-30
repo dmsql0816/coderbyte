fdeb408c4d63c919f0dd440ee2f16e55d69d36af
dmsql0816 <dmsql0816@daum.net>
2015-07-30 16:01:12 +0900

    Create WordCount.rb

---
+++ WordCount.rb
@@ -0,0 +1,15 @@
+def WordCount(str)
+
+  return str.split.length
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+WordCount(STDIN.gets)  
+
+
+
+
+
+
